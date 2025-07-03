114cd81b5607e68066a1d1c54c1ba0dc5f904dac
Lauren Wiemers <laurenwiemers12@gmail.com>
2025-07-03 18:45:40 +0000

    commit

--- models/staging/raw/stg_raw__store_information.sql
+++ models/staging/raw/stg_raw__store_information.sql
@@ -11,9 +11,9 @@ renamed as (
     select
         id_store,
         date_created,
-        dim_zipcode,
-        dim_country,
-        dim_currency
+        dim_zipcode AS zipcode,
+        dim_country AS country,
+        dim_currency AS currency
 
     from source
 
